fd10eddc439777870e733a8b2bb2c41eac137a62
Dan <dwoj714@gmail.com>
2023-12-16 20:41:51 -0500

    Added electric visual to grapple using beam3D

--- components/Weapons/grapple_gun.gd
+++ components/Weapons/grapple_gun.gd
@@ -7,6 +7,9 @@ var grapple_hit : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$SimpleBeam.visible = false
+	$Beam3D.visible = false
+	projectile_type = null
 	pass # Replace with function body.
 
 
@@ -16,22 +19,24 @@ func _process(delta):
 	if is_firing && grapple_hit:
 		#$LaserBeam.update_position(global_position, grapple_point, transform.basis.y)
 		var distance = global_position.distance_to(grapple_point)
-		$SimpleBeam.set_length(distance)
-		$SimpleBeam.look_at(grapple_point)
+		#$SimpleBeam.set_length(distance)
+		#$SimpleBeam.look_at(grapple_point)
 		get_owning_controller().velocity += (grapple_point - global_position).normalized() * grapple_accel * delta
-		
 
 func start_fire():
 	super.start_fire()
 	grapple_hit = false
 	_handle_fire()
-	$SimpleBeam.visible = grapple_hit
+	#$SimpleBeam.visible = grapple_hit
+	$Beam3D.visible = grapple_hit
 
 func stop_fire():
 	super.stop_fire()
-	$SimpleBeam.visible = false
+	#$SimpleBeam.visible = false
+	$Beam3D.visible = false
 
 func _on_hitscan_hit(hit_dict: Dictionary):
 	grapple_point = hit_dict["position"]
 	grapple_hit = true
+	$Beam3D.target_point = grapple_point
 	pass
